009010fcf9d56c3f34ba67e19fa668b065729b2c
John <601473400@qq.com>
2019-05-21 22:36:12 +0800

    02,03页

--- postcss.config.js
+++ postcss.config.js
@@ -10,6 +10,10 @@ module.exports = {
         'Android > 4.4'
       ],
       cascade: true
+    },
+    'postcss-pxtorem': {
+      'rootValue': 50,  // 根元素大小px
+      'propList': ['*']  // *表示通用,所有属性都转为rem
     }
   }
 }
